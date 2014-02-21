18e2ccaa0d951925d63615cc16bf0a363d46acca
Hunter Nield <hunternield@gmail.com>
2014-02-21 15:28:46 +0800

    Handle syslog as a filter

--- logstash/run.sh
+++ logstash/run.sh
@@ -23,6 +23,19 @@ input {
     type => "$LUMBERJACK_TAG"
   }
 }
+filter {
+  if [type] == "syslog" {
+    grok {
+      match => { "message" => "%{SYSLOGTIMESTAMP:syslog_timestamp} %{SYSLOGHOST:syslog_hostname} %{DATA:syslog_program}(?:\[%{POSINT:syslog_pid}\])?: %{GREEDYDATA:syslog_message}" }
+      add_field => [ "received_at", "%{@timestamp}" ]
+      add_field => [ "received_from", "%{host}" ]
+    }
+    syslog_pri { }
+    date {
+      match => [ "syslog_timestamp", "MMM  d HH:mm:ss", "MMM dd HH:mm:ss" ]
+    }
+  }
+}
 output {
   stdout {
       debug => true
