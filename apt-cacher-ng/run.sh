0291d7891407f0c289105d521169b653ee32beca
Alistair Israel <aisrael@gmail.com>
2014-05-23 14:49:01 +0800

    Added interactive comment.

--- apt-cacher-ng/run.sh
+++ apt-cacher-ng/run.sh
@@ -3,7 +3,10 @@
 # Make sure the following directories are present on the Docker host:
 #    /var/cache/apt-cacher-ng
 #    /var/log/apt-cacher-ng
-#
+
+# To run bash interactively:
+# docker run -i -t -v /var/log/apt-cacher-ng:/var/log/apt-cacher-ng -v /var/cache/apt-cacher-ng:/var/cache/apt-cacher-ng -p 3142:3142 --entrypoint /bin/bash acaleph/apt-cacher-ng
+
 # Taken from ps -afe|grep apt-cacher-ng while service is running
 docker run -d --name apt-cacher-ng \
 -v /var/log/apt-cacher-ng:/var/log/apt-cacher-ng \
